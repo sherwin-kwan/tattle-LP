38984f08b75407452c348e192974106a6ad03e0e
Kyle Tycholiz <tycholiz22@hotmail.com>
2019-04-14 12:42:30 -0700

    integrate typed.js. adjustments needed still

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,5 +1,7 @@
+import Typed from 'typed.js';
 $(document).ready(function () {
-  console.log("jquery loaded!!!");
+	console.log(Typed)
+	console.log("jquery loaded!!!");
 
   $("#js-signUpForm").click(() => {
     console.log('clicked!')
@@ -13,38 +15,11 @@ $(document).ready(function () {
 			'slow');
 	});
 
-	// var element_position = $('#js-signUpForm').offset().top;
-	// $(window).on('scroll', function () {
-	// 	var y_scroll_pos = window.pageYOffset;
-	// 	var scroll_pos_test = element_position;
-	// 	console.log(scroll_pos_test);
-	// 	// console.log(y_scroll_pos);
-	// 	if (y_scroll_pos > scroll_pos_test) {
-	// 		//do stuff
-	// 	}
-	// });
-
-	// $.fn.isInViewport = function () {
-	// 	var elementTop = $(this).offset().top;
-	// 	var elementBottom = elementTop + $(this).outerHeight();
-
-	// 	var viewportTop = $(window).scrollTop();
-	// 	var viewportBottom = viewportTop + $(window).height();
-
-	// 	return elementBottom > viewportTop && elementTop < viewportBottom;
-	// };
-
-	// $(function () {
-	// 	// var eTop = $('signUpForm').offset().top; //get the offset top of the element
-	// 	// log(eTop - $(window).scrollTop()); //position of the ele w.r.t window
-
-	// 	$(window).scroll(function () { //when window is scrolled
-	// 		// log(eTop - $(window).scrollTop());
-	// 		// eTop <= $(window).scrollTop() && shrink();
-	// 		console.log($(this).isInViewport())
-	// 	});
-	// });
-
-
+	var typed = new Typed('#type', {
+		strings: ["you've got your passion^1000\n you've got your city^500\n^500\n^500\n now share it with the world"],
+		typeSpeed: 40,
+		backSpeed: 40,
+		// loop: true
+	});
 
 });
