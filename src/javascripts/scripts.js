84be0896c60525ba0013eae0b8eeab53be41e5cd
Kyle Tycholiz <tycholiz22@hotmail.com>
2019-04-14 14:32:23 -0700

    granim embedded. colors need adjusting still

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,6 +1,8 @@
 import Typed from 'typed.js';
 import AOS from 'aos'
 import 'aos/dist/aos.css';
+import Granim from 'granim'
+AOS.init();
 
 $(document).ready(function () {
 	console.log("jquery loaded!!!");
@@ -17,13 +19,27 @@ $(document).ready(function () {
 			'slow');
 	});
 
+	//TYPED.js
 	var typed = new Typed('#js-typed', {
 		strings: ["you've got your passion.^1000\n you've got your city.^300\n^300\n^300\n now, ^500 let's share it\n with the world^500."],
 		typeSpeed: 30,
 		backSpeed: 40,
 	});
 
-	AOS.init();
-	console.log(AOS)
+	//GRANIM.js
+	var granimInstance = new Granim({
+		element: '#granim-canvas',
+		name: 'granim',
+		opacity: [1, 1],
+		states: {
+			"default-state": {
+				gradients: [
+					['#834D9B', '#D04ED6'],
+					['#1CD8D2', '#93EDC7']
+				]
+			}
+		}
+	});
+
 
 });
